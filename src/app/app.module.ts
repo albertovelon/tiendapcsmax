63bf715e11ef1945c868821d290e6257015ea0db
albertovelon <https://albertovelon@bitbucket.org>
2017-09-26 12:02:29 -0500

    version 2 componentes clientes y proveedores, con opciones de menu funcionando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,23 @@
+import { RouterModule, Routes } from '@angular/router';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { ClientesModule } from './clientes/clientes.module';
+import { AdicionaComponent as AdicionaComponent} from './proveedores/adiciona/adiciona.component';
+
+import { IndexComponent } from './clientes/index/index.component';
 
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { CurrentOptionComponent } from './current-option/current-option.component';
+import { ProveedoresModule } from './proveedores/proveedores.module';
+
+const appRoutes : Routes = [
+  { path: 'clientes/index', component: IndexComponent },
+  { path: 'proveedores/adiciona', component: AdicionaComponent }
+
+];
 
 @NgModule({
   declarations: [
@@ -15,7 +27,11 @@ import { CurrentOptionComponent } from './current-option/current-option.componen
   ],
   imports: [
     BrowserModule,
-    ClientesModule
+    ClientesModule,
+    ProveedoresModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
